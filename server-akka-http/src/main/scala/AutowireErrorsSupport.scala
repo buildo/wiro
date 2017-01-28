a6039f5a7547c13288f5b61504ad70a642b6a393
Claudio Caletti <claudio@buildo.io>
2017-01-28 15:22:56 +0100

    Separate autowire errors handling module

---
+++ server-akka-http/src/main/scala/AutowireErrorsSupport.scala
@@ -0,0 +1,64 @@
+package wiro.server.akkaHttp
+
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.AuthenticationFailedRejection.CredentialsRejected
+import akka.http.scaladsl.model.headers.HttpChallenges
+import akka.http.scaladsl.model._
+
+object AutowireErrorSupport {
+  def handleUnwrapErrors(
+    throwable: Throwable
+  ) = throwable match {
+    case autowire.Error.InvalidInput(xs) =>
+      handleAutowireInputErrors(xs)
+    case e: autowire.Error.InvalidInput =>
+      complete(HttpResponse(
+        status = StatusCodes.UnprocessableEntity,
+        entity = "Unprocessable entity"
+      ))
+    case _: scala.MatchError =>
+      complete(HttpResponse(
+        status = StatusCodes.MethodNotAllowed,
+        entity = "Method not found"
+      ))
+    case _: Exception =>
+      complete(HttpResponse(
+        status = StatusCodes.InternalServerError,
+        entity = "Internal Error"
+      ))
+  }
+
+  private[this] def handleMissingParamErrors(
+    param: String
+  ): StandardRoute = param match {
+    case "token" => reject(AuthenticationFailedRejection(
+      cause = CredentialsRejected,
+      challenge = HttpChallenges.basic("api")
+    ))
+    case "actionQuery" => complete(HttpResponse(
+      status = StatusCodes.MethodNotAllowed,
+      entity = s"Method not allowed"
+    ))
+    case "actionCommand" => complete(HttpResponse(
+      status = StatusCodes.MethodNotAllowed,
+      entity = s"Method not allowed"
+    ))
+    case _ => complete(HttpResponse(
+      status = StatusCodes.UnprocessableEntity,
+      entity = s"Missing parameter $param from input"
+    ))
+  }
+
+  private[this] def handleAutowireInputErrors(
+    xs: autowire.Error.Param
+  ): StandardRoute = xs match {
+    case autowire.Error.Param.Missing(param) =>
+      handleMissingParamErrors(param)
+    case autowire.Error.Param.Invalid(param, ex) =>
+      complete(HttpResponse(
+        status = StatusCodes.UnprocessableEntity,
+        entity = s"Missing parameter $param from input"
+      ))
+  }
+}
--- server-akka-http/src/main/scala/RPCRouter.scala
+++ server-akka-http/src/main/scala/RPCRouter.scala
@@ -1,13 +1,12 @@
 package wiro.server.akkaHttp
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.headers.HttpChallenges
 import akka.http.scaladsl.server._
-import akka.http.scaladsl.server.AuthenticationFailedRejection.CredentialsRejected
 import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
 import de.heikoseeberger.akkahttpcirce.CirceSupport._
 
 import wiro.models._
 import FailSupport._
+import AutowireErrorSupport._
 
 import scala.language.implicitConversions
 
@@ -107,56 +106,6 @@ object RouteGenerators {
         case scala.util.Failure(f) => handleUnwrapErrors(f)
       }
     }
-
-    private[this] def handleUnwrapErrors(
-      throwable: Throwable
-    ) = throwable match {
-      case autowire.Error.InvalidInput(xs) =>
-        handleAutowireInputErrors(xs)
-      case e: autowire.Error.InvalidInput =>
-        complete(HttpResponse(
-          status = StatusCodes.UnprocessableEntity,
-          entity = "Unprocessable entity"
-        ))
-      case _: scala.MatchError =>
-        complete(HttpResponse(
-          status = StatusCodes.MethodNotAllowed,
-          entity = "Method not found"
-        ))
-      case _: Exception =>
-        complete(HttpResponse(
-          status = StatusCodes.InternalServerError,
-          entity = "Internal Error"
-        ))
-      }
-  }
-
-  private[this] def handleAutowireInputErrors(
-    xs: autowire.Error.Param
-  ): StandardRoute = xs match {
-    case autowire.Error.Param.Missing(param) => param match {
-      case "token" => reject(AuthenticationFailedRejection(
-        cause = CredentialsRejected,
-        challenge = HttpChallenges.basic("api")
-      ))
-      case "actionQuery" => complete(HttpResponse(
-        status = StatusCodes.MethodNotAllowed,
-        entity = s"Method not allowed"
-      ))
-      case "actionCommand" => complete(HttpResponse(
-        status = StatusCodes.MethodNotAllowed,
-        entity = s"Method not allowed"
-      ))
-      case _ => complete(HttpResponse(
-        status = StatusCodes.UnprocessableEntity,
-        entity = s"Missing parameter $param from input"
-      ))
-    }
-    case autowire.Error.Param.Invalid(param, ex) =>
-      complete(HttpResponse(
-        status = StatusCodes.UnprocessableEntity,
-        entity = s"Missing parameter $param from input"
-      ))
   }
 
   private[this] def addTokenToParams(
