633bc4d0b2ecaa564bd5a80e7fdc5d3930675ac9
Claudio Caletti <claudio@buildo.io>
2017-05-01 19:39:36 +0200

    Fix tests accordingly

--- serverAkkaHttp/src/test/scala/TestController.scala
+++ serverAkkaHttp/src/test/scala/TestController.scala
@@ -13,7 +13,7 @@ import wiro.server.akkaHttp._
 import wiro.server.akkaHttp.FailSupport._
 import wiro.server.akkaHttp.RouteGenerators._
 
-object TestController {
+object TestController extends MetaDataMacro {
   case class UserNotFound(userId: Int)
   case class Conflict(userId: Int)
   case object GenericError
@@ -51,34 +51,30 @@ object TestController {
     @query
     def find(id: Int): Future[Either[UserNotFound, User]]
 
-    @command("insert")
+    @command(name = Some("insert"))
     def insertUser(id: Int, user: User): Future[Either[Conflict, Ok]]
 
-    @query(name = "number")
+    @query(name = Some("number"))
     def usersNumber(): Future[Either[GenericError, Int]]
   }
 
   private[this] class UserControllerImpl(implicit
     ec: ExecutionContext
   ) extends UserController {
-    @command
     def update(id: Int, user: User): Future[Either[UserNotFound, Ok]] = Future {
       if (id == 1) Right(Ok("happy update"))
       else Left(UserNotFound(id))
     }
 
-    @query
     def find(id: Int): Future[Either[UserNotFound, User]] = Future {
       if (id == 1) Right(User(id, "foo"))
       else Left(UserNotFound(id))
     }
 
-    @command("insert")
     def insertUser(id: Int, user: User): Future[Either[Conflict, Ok]] = Future {
       Right(Ok("inserted!"))
     }
 
-    @query(name = "number")
     def usersNumber(): Future[Either[GenericError, Int]] = Future {
       Right(1)
     }
@@ -86,8 +82,9 @@ object TestController {
 
   private[this] val userController = new UserControllerImpl(): UserController
   def userRouter = new RouteGenerator[UserController] {
-    val routes = route[UserController](userController)
-    val tp = typePath[UserController]
+    override val routes = route[UserController](userController)
+    override val tp = typePath[UserController]
+    override val methodsMetaData = deriveMetaData[UserController]
     override val path = derivePath[UserController]
   }
 }
