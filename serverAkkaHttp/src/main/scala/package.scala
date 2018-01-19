c1b0c7eb48743b7b7e798554fcbabba5cb603b56
Claudio Caletti <claudio@buildo.io>
2018-01-19 11:53:08 +0100

    Add configuration case class

--- serverAkkaHttp/src/main/scala/package.scala
+++ serverAkkaHttp/src/main/scala/package.scala
@@ -4,6 +4,8 @@ import akka.http.scaladsl.model.HttpResponse
 import io.circe.Json
 
 package object akkaHttp {
+  final case class ReferenceConfig(routesPrefix: Option[String], ciao: String)
+
   trait ToHttpResponse[A] {
     def response(a: A): HttpResponse
   }
