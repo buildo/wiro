0b522560aac453263b254926ced1d73d765b3e35
Claudio Caletti <caletti.claudio@gmail.com>
2018-09-19 16:14:07 +0200

    Add path prefix to client tests

--- clientAkkaHttp/src/test/scala/RPCTestClient.scala
+++ clientAkkaHttp/src/test/scala/RPCTestClient.scala
@@ -1,8 +1,9 @@
 package wiro
 
 import akka.http.scaladsl.model.HttpRequest
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.testkit.{ RouteTest, TestFrameworkInterface }
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.testkit._
 
 import cats.implicits._
 import client.akkaHttp.{ RPCClient, RPCClientContext }
@@ -10,9 +11,10 @@ import client.akkaHttp.{ RPCClient, RPCClientContext }
 import scala.concurrent.Future
 
 trait RPCRouteTest extends RouteTest { this: TestFrameworkInterface =>
+  val prefix = Some("test")
   class RPCClientTest(ctx: RPCClientContext[_], route: Route)
-    extends RPCClient(config = Config("localhost", 80), ctx = ctx){
+    extends RPCClient(config = Config("localhost", 80), prefix = prefix, ctx = ctx){
       override private[wiro] def doHttpRequest(request: HttpRequest) =
-        (request ~> route).response.pure[Future]
+        (request ~> pathPrefix(prefix.get)(route)).response.pure[Future]
   }
 }
